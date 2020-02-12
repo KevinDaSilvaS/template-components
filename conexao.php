db3ff1d6c04a3b7234eb1868574e6849f181064d
Kevin da Silva <kevin.kds80@gmail.com>
2020-02-12 17:09:27 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,7 +1,5 @@
 <?php
-
-
-$mysqli_connection =new MySQLi(HOST, USER, PASSWOR, DB,PORT(OPTIONAL)) ;
+$mysqli_connection =new MySQLi(HOST, USER, PASSWORD, DB,PORT(OPTIONAL)) ;
 if ($mysqli_connection->connect_error) {
    
     echo "Error:".$mysqli_connection->connect_error;
