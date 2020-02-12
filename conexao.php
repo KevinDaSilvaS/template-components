77b2e5861eb78d52f444424e5c6e0bf770d12bb7
Kevin da Silva <kevin.kds80@gmail.com>
2020-02-12 17:08:56 -0300

    english version

--- conexao.php
+++ conexao.php
@@ -1,14 +1,11 @@
 <?php
-define('HOST', 'localhost');
-define('USUARIO', 'root');
-define('SENHA', '');
-define('DB', 'sevite');
 
-$mysqli_connection =new MySQLi(HOST, USUARIO, SENHA, DB,3308) ;
+
+$mysqli_connection =new MySQLi(HOST, USER, PASSWOR, DB,PORT(OPTIONAL)) ;
 if ($mysqli_connection->connect_error) {
-    # code...
-    echo "desconectado Erro:".$mysqli_connection->connect_error;
+   
+    echo "Error:".$mysqli_connection->connect_error;
 }else {
-    //echo "conectado";
+    //echo "conected";
 }
 ?>
